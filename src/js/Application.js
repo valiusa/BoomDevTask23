0d52a354211931081b49aea78f487ccffd8dcca7
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-08 22:26:56 +0200

    bug fix

--- src/js/Application.js
+++ src/js/Application.js
@@ -27,14 +27,15 @@ export default class Application extends EventEmitter {
     _loading = `<progress></progress>`;
 
     async _load() {
-        let task = await fetch("https://example.com");
+        let task = await fetch("https://swapi.boom.dev/api/planets").then(
+            (response) => {
+                const progress = document.querySelector(".pregress");
+                progress.style.display = "none";
+            }
+        );
     }
 
-    _create() {}
-    _startLoading() {}
-    _stopLoading() {}
-
-    _render({ name, terrain, population }) {
+    _create({ name, terrain, population }) {
         return `
 <article class="media">
   <div class="media-left">
@@ -54,4 +55,27 @@ export default class Application extends EventEmitter {
 </article>
     `;
     }
+    _startLoading() {}
+    _stopLoading() {}
+
+    //     _render({ name, terrain, population }) {
+    //         return `
+    // <article class="media">
+    //   <div class="media-left">
+    //     <figure class="image is-64x64">
+    //       <img src="${image}" alt="planet">
+    //     </figure>
+    //   </div>
+    //   <div class="media-content">
+    //     <div class="content">
+    //     <h4>${name}</h4>
+    //       <p>
+    //         <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+    //         <br>
+    //       </p>
+    //     </div>
+    //   </div>
+    // </article>
+    //     `;
+    //     }
 }
